e8c0b0e3c42d31e6b9c9033c0059a58dfa03c658
GiorgiKalatozi <giorgikalatoz@gmail.com>
2023-12-28 12:44:08 +0400

    refacotr: main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { CrewMember } from "./modules/CrewMember";
 import { CruiseCompany } from "./modules/CruiseCompany";
 import { CruiseCompanyWithNotification } from "./modules/CruiseCompanyWithNotification";
 import { Discount } from "./modules/Discount";
@@ -6,7 +7,8 @@ import { Payment } from "./modules/Payment";
 import { RecreationalActivity } from "./modules/RecreationalActivity";
 import { Route } from "./modules/Route";
 import { BookingService } from "./services/BookingService";
-import { CabinType } from "./types/enums";
+import { TaskService } from "./services/TaskService";
+import { CabinType, CrewMemberType } from "./types/enums";
 
 const passenger = new Passenger(
   "Monkey D. Luffy",
@@ -30,10 +32,6 @@ const passenger = new Passenger(
 //   }
 // );
 
-// const recreationalActivities: RecreationalActivity[] = [
-//   { name: "Swimming Pool", price: 80 },
-//   { name: "We go gym", price: 50 },
-// ];
 const recreationalActivities: RecreationalActivity[] = [
   new RecreationalActivity("Swimming", "Enjoy a refreshing swim in the pool"),
   new RecreationalActivity("Fishing", "Hunt Sharks and Whales"),
@@ -48,24 +46,6 @@ const cruiseCompany = new CruiseCompany(
   CabinType.Business,
   recreationalActivities,
   ADVANCE_BOOKING_DISCOUNT_DAYS
-);
-
-// Set a route with two legs
-cruiseCompany.setRoute(
-  "Batumi to Alexandria",
-  ["Batumi", "Istanbul", "Antalya", "Alexandria"],
-  ["Alexandria", "Antalya", "Istanbul", "Batumi"],
-  new Date("2024-01-01"),
-  new Date("2024-01-10")
-);
-
-// Set another route with different back route
-cruiseCompany.setRoute(
-  "Batumi to Alexandria (Alternative Route)",
-  ["Batumi", "Sochi", "Antalya", "Alexandria"],
-  ["Alexandria", "Antalya", "Sochi", "Batumi"],
-  new Date("2023-02-01"),
-  new Date("2023-02-10")
 );
 
 const payment = new Payment(500, true);
@@ -88,26 +68,24 @@ const booking = new BookingService(
   ["Excursions"]
 );
 
+const task1 = new TaskService("Check navigation systems");
+const task2 = new TaskService("Inspect engine components");
+const task3 = new TaskService("Learn SOLID");
+const task4 = new TaskService("Learn Design Patterns");
+
+const crewMember1 = new CrewMember("John", CrewMemberType.Captain, task1);
+const crewMember2 = new CrewMember("Jane", CrewMemberType.Chef, task2);
+const crewMember3 = new CrewMember("Bob", CrewMemberType.Mechanic, task3);
+const crewMember4 = new CrewMember("Bob", CrewMemberType.Cleaner, task4);
+
 // Book the cruise
 cruiseCompany.bookCruise(booking);
 
-// Create crew members
-// const captain = new CrewMember("Jack Sparrow", CrewMemberType.Captain);
-// const mechanic = new CrewMember("Jason Statham", CrewMemberType.Mechanic);
-// const chef = new CrewMember("Gordon Ramsay", CrewMemberType.Chef);
-// const cleaner = new CrewMember("David Gogins", CrewMemberType.Cleaner);
-
 // Add crew members
-// cruiseCompany.addCrewMember(captain);
-// cruiseCompany.addCrewMember(mechanic);
-// cruiseCompany.addCrewMember(chef);
-// cruiseCompany.addCrewMember(cleaner);
-
-// Assign tasks to crew members
-// captain.assignGeneralTask(new Task("Lead the team to victory"));
-// mechanic.assignGeneralTask(new Task("Inspect engine"));
-// chef.assignDailyTask(new Task("Plan meals"));
-// cleaner.assignDailyTask(new Task("Clean everything"));
+cruiseCompany.addCrewMember(crewMember1);
+cruiseCompany.addCrewMember(crewMember2);
+cruiseCompany.addCrewMember(crewMember3);
+cruiseCompany.addCrewMember(crewMember4);
 
 // Get all crew members and their tasks
 const crewMembers = cruiseCompany.getCrewMembers();
