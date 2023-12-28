f58f7993ed7d19f2c09163c111fb905e880c7ed1
GiorgiKalatozi <giorgikalatoz@gmail.com>
2023-12-28 17:09:23 +0400

    feat: add discount methods'

--- src/modules/Discount.ts
+++ src/modules/Discount.ts
@@ -4,4 +4,24 @@ export class Discount {
     public percentage: number,
     public expirationDate: Date
   ) {}
+
+  isStillValid(): boolean {
+    const currentDate = new Date();
+    return currentDate <= this.expirationDate;
+  }
+
+  applyDiscount(price: number): number {
+    if (this.isStillValid()) {
+      const discountAmount = (this.percentage / 100) * price;
+      return price - discountAmount;
+    } else {
+      console.log(`${this.name} discount has expired.`);
+      return price;
+    }
+  }
+
+  getDetails(): string {
+    const validityStatus = this.isStillValid() ? "valid" : "expired";
+    return `${this.name}: ${this.percentage}% discount (Status: ${validityStatus})`;
+  }
 }
